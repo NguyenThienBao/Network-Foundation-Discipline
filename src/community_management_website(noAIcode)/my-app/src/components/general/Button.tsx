4b8c6aa611a533ff24323bb4ac24d0bb38b86884
Nguyen Thien Bao <nguyenthienbao@192.168.1.241>
2026-06-19 16:20:51 +0700

    fix(community): remove noAICode since using Gemini

--- src/community_management_website(noAIcode)/my-app/src/components/general/Button.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ name }: { name: string }) {
-    return (
-        <button className="bg-sfalightblue custom-text-normal px-4 py-2 rounded border-solid border-1 border-sfadarkgray">
-            {name}
-        </button>
-    );
-}

390d0551025d8cbd9ae045c74d1584936893be69
Nguyen Thien Bao <nguyenthienbao@192.168.1.14>
2026-06-27 20:37:03 +0700

    fix(main):delete old project

--- src/community_management_website(noAIcode)/my-app/src/components/general/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-2 mt-auto">
-      <div className="container mx-auto text-center">
-        <p className="text-xs">&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
